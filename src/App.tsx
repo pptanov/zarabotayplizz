5d332ad8bbd27fe7c8b3c99c51335797031b1041
pptanov <tanov.pavel20156@gmail.com>
2024-03-17 02:05:08 +0300

    xx

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,71 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Table} from "antd"
+import {Button} from "antd"
+import axios from 'axios'
+import { ColumnsType } from 'antd/es/table'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [page, setPage] = useState(1);
+  let [dataSource, setDataSource] = useState();
+  let [dataSourceNext, setDataSourceNext] = useState();
+
+  interface DataType {
+    id: string;
+    title: string;
+  }
+  
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+  ];
+
+  const limit = 10;
+  
+  const getPagedData = async (page: number, limit: number) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
+    setDataSource(response.data);
+  }
+  const getPagedDataNext = async (page: number, limit: number) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
+    setDataSourceNext(response.data);
+  }
+
+
+  getPagedData(page, limit);
+
+
+  function isDisabledBack(){
+    return (page <= 1) ? true : false;
+  }
+
+  function isDisabledForward(){
+    getPagedDataNext(page+1, limit);
+    return (!dataSourceNext) ? true : false;
+  }
+
+
+  let valueBackButton = isDisabledBack();
+  let valueForwardButton = isDisabledForward();
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Table pagination={false} dataSource={dataSource} columns={columns} />
+      <Button onClick={() => setPage(page-1)} disabled={valueBackButton}>Назад</Button>
+      <Button onClick={() => setPage(page+1)} disabled={valueForwardButton}>Вперед</Button>
+      <p>Page: {page}</p>
     </>
   )
 }
